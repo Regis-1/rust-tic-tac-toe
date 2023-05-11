68c1cee5cbcbb79e96ba9c969c26bd3682d7117e
Regis-1 <jdudzinski101@gmail.com>
2023-05-11 22:00:24 +0200

    welcome msg + game mode selection

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,12 @@
+use std::io::{self, Write};
+
 struct Marks {
     x: char,
     o: char,
 }
 
-enum Modes {
+#[derive(Debug)]
+pub enum Modes {
     Ai,
     Human,
     Online,
@@ -20,14 +23,37 @@ impl App {
         }
     }
 
-    pub fn mode_select(&self) {
-        let welcome_msg = "Welcome to Rusty Tic-Tac-Toe!
-Please select preffered game mode:
+    pub fn show_menu(&self) {
+        println!("Welcome to Rusty Tic-Tac-Toe!");
+        self.show_mode_selection();
+    }
+
+    fn show_mode_selection(&self) {
+        let modes_list = "Please select preffered game mode:
 [1] - Player vs Ai
 [2] - Player vs Human
 [3] - Online match";
         let prompt = "-> ";
+        
+        print!("{modes_list}\n\n{prompt}");
+        io::stdout().flush().unwrap();
+    }
 
-        print!("{welcome_msg}\n\n{prompt}");
+    pub fn mode_select(&self) -> Result<Modes, std::io::Error> {
+        loop {
+            let mut player_input = String::new();
+            io::stdin().read_line(&mut player_input)?;
+
+            match player_input.chars().next().unwrap() {
+                '1' => return Ok(Modes::Ai),
+                '2' => return Ok(Modes::Human),
+                '3' => return Ok(Modes::Online),
+                _ => self.show_mode_selection(),
+            }
+        }
+    }
+
+    pub fn run_game(&self, mode: Modes) {
+        println!("Running game with the {:?} mode...", mode);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod app;
+
 use app::App;
 
 fn main() {
     let app = App::default();
 
-    app.mode_select();
+    app.show_menu();
+    let selected_mode = app.mode_select().unwrap_or_else(|err| {
+        eprintln!("ERR: Error while mode selection -> {err}");
+        std::process::exit(-1);
+    });
+
+    app.run_game(selected_mode);
 }
